bb76036f9953ea10e5d452a7ed2d8a995e630f3a
Pablo Pedraza <wensel84@gmail.com>
2023-04-24 08:36:47 -0300

    cambio color spinner

--- src/services/consulado.js
+++ src/services/consulado.js
@@ -1,16 +1,12 @@
 //const API_URL = 'http://localhost:5000/turnos-pasaporte'
 const API_URL = 'https://consulta-consulado-api.vercel.app/turnos-pasaporte'
 export const getNextOpeningPassport = async () => {
-  console.log('antes del fetch')
   try {
     const response = await fetch(API_URL)
-
-    console.log('ya hice el fetch')
     if (!response.ok) {
       throw new Error('Error al hacer fetch')
     }
-    const data = await response.json()
-    return data
+    return await response.json()
   } catch (e) {
     console.log('error en el fetch', e)
   }
