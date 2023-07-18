5fbdb8e311d8c87aeddee4dcba910d63dad34665
Pablo Pedraza <wensel84@gmail.com>
2023-07-18 12:22:04 -0300

    agrego cafectio component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,15 @@
+import { Cafecito } from "./Cafecito";
+
 export function Footer({ author, github }) {
   return (
-    <footer className="text-xs bottom-0 left-0 right-0 2xl:mt-10 2xl:pb-20 lg:mt-3  md:mt-3 md:pb-2 mt-10 pb-5 text-center opacity-60  text-white/80">
+    <footer className="flex flex-col text-xs bottom-0 left-0 right-0 2xl:mt-10 2xl:pb-20 lg:mt-3  md:mt-3 md:pb-2 mt-10 pb-5 text-center opacity-60  text-white/80 justify-center">
+      <div>
       {' '}
       Desarrollado por{' '}
       <a
         className="text-white hover:underline"
         target="_blank"
-        href={`mailto:pabloj.pedraza@gmail.com`}
+        href={`mailto:pabloj.pedraza@gmail.com`} rel="noreferrer"
       >
         {author}
       </a>{' '}
@@ -14,10 +17,12 @@ export function Footer({ author, github }) {
       <a
         className="text-white hover:underline"
         target="_blank"
-        href={`https://github.com/${github}`}
+        href={`https://github.com/${github}`} rel="noreferrer"
       >
         Github
       </a>
+      </div>
+      <Cafecito/>
     </footer>
   )
 }
